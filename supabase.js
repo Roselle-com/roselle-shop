b2122afd87953c78fcb124a5bfef854525870fec
Roselle-com <farimah.ansari.pqsd@gmail.com>
2026-06-29 19:01:34 +0330

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,53 @@
+const SUPABASE_URL = "https://sgggoawcwzjuxnvpekjk.supabase.co";
+const SUPABASE_KEY = "sb_publishable_cvHHMTAfS1zWR5Btm6qy3g__hZo9dSQ";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+async function getProducts(){
+
+    const { data, error } = await supabaseClient
+        .from("products")
+        .select("*");
+
+    if(error){
+        console.log(error);
+        alert(error.message);
+        return;
+    }
+
+    const productsBox = document.querySelector(".products");
+
+    data.forEach(product=>{
+
+        productsBox.innerHTML += `
+        <div class="card">
+
+            <img src="${product.image}" alt="${product.name}">
+
+            <h3>${product.name}</h3>
+
+            <p>${product.price} تومان</p>
+
+            <button>
+                افزودن به سبد خرید
+            </button>
+
+        </div>
+        `;
+
+    });
+
+    const buttons = document.querySelectorAll("button");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            alert("محصول به سبد خرید اضافه شد.");
+        });
+    });
+
+}
+
+getProducts();
